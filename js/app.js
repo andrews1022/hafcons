ed6938fa14e7a750725a6329a59233496033a8e9
Andrew Shearer <andrewshearerdev@gmail.com>
2022-11-02 10:56:32 -0700

    Quick formatting with new Prettier & ESLint settings

--- js/app.js
+++ js/app.js
@@ -1,32 +1,32 @@
-const cardsContainer = document.querySelector('.cards-container'); // container to insert individual cards into
-const cards = document.querySelectorAll('.card');
+const cardsContainer = document.querySelector(".cards-container");
+const cards = document.querySelectorAll(".card");
 
 const createCards = async () => {
-	const response = await fetch('./data/brands.json');
-	const brands = await response.json();
+  const response = await fetch("./data/brands.json");
+  const brands = await response.json();
 
-	brands.forEach((el) => {
-		const card = document.createElement('div');
-		const hexes = el.hexes;
+  brands.forEach((el) => {
+    const card = document.createElement("div");
+    const hexes = el.hexes;
 
-		card.classList.add('card');
-		card.innerHTML = `
+    card.classList.add("card");
+    card.innerHTML = `
       <div class="card__header">
         <i class="fab fa-${el.class} card__icon"></i>
         <h3 class="card__heading">${el.heading}</h3>
       </div>
     `;
 
-		hexes.forEach((el) => {
-			const para = document.createElement('p');
-			para.classList.add('card__color-value');
-			para.innerHTML = `${el} <i class="far fa-copy card__color-icon"></i>`;
-			para.style.backgroundColor = el;
-			card.appendChild(para);
-		});
+    hexes.forEach((el) => {
+      const para = document.createElement("p");
+      para.classList.add("card__color-value");
+      para.innerHTML = `${el} <i class="far fa-copy card__color-icon"></i>`;
+      para.style.backgroundColor = el;
+      card.appendChild(para);
+    });
 
-		cardsContainer.appendChild(card);
-	});
+    cardsContainer.appendChild(card);
+  });
 };
 
-document.addEventListener('DOMContentLoaded', createCards);
+document.addEventListener("DOMContentLoaded", createCards);
